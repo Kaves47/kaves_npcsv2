45ef1e9e5f1a99ee93d25afd4030d162b22f71f5
Kaves47 <69982652+Kaves47@users.noreply.github.com>
2022-06-21 12:26:58 +0300

    Update config.lua

--- kaves_npcsv2/config.lua
+++ kaves_npcsv2/config.lua
@@ -2,19 +2,19 @@ Config = {}
 
 Config.Kaves = {
 
-	[1] = { handle = nil, model ="s_m_m_paramedic_01", coords = vector3(584.64, 2723.0, 42.08), heading = 52.5, text = "Brian Jeff", weapon = "WEAPON_UNARMED", anim = { type = 1, name = "CODE_HUMAN_CROSS_ROAD_WAIT"} }, -- SENARYO ÖRNEK
-	[2] = { handle = nil, model ="s_m_y_marine_03", coords = vector3(561.68, 2718.76, 44.32), heading = 191.55, text = "Robert Downey", weapon = "WEAPON_ASSAULTRIFLE", anim = { type = 0, name = ""} }, -- SİLAHLI NPC ÖRNEK
-	[3] = { handle = nil, model ="a_m_m_hillbilly_01", coords = vector3(556.12, 2726.2, 42.08), heading = 97.12, text = "Richard Murphy", weapon = "WEAPON_UNARMED", anim = { type = 2, dict = "mini@repair", name = "fixing_a_ped"} }, -- ANİMASYON ÖRNEK
+	[1] = { handle = nil, model ="s_m_m_paramedic_01", coords = vector3(584.64, 2723.0, 42.08), heading = 52.5, text = "Brian Jeff", weapon = "WEAPON_UNARMED", anim = { type = 1, name = "CODE_HUMAN_CROSS_ROAD_WAIT"} }, -- scene
+	[2] = { handle = nil, model ="s_m_y_marine_03", coords = vector3(561.68, 2718.76, 44.32), heading = 191.55, text = "Robert Downey", weapon = "WEAPON_ASSAULTRIFLE", anim = { type = 0, name = ""} }, -- armored npc
+	[3] = { handle = nil, model ="a_m_m_hillbilly_01", coords = vector3(556.12, 2726.2, 42.08), heading = 97.12, text = "Richard Murphy", weapon = "WEAPON_UNARMED", anim = { type = 2, dict = "mini@repair", name = "fixing_a_ped"} }, -- anim
 	
 }
 
 --[[
 
-type = 0 # boş
+type = 0 # empty
 
-type = 1 # senaryo
+type = 1 # scene
 
-type = 2 # animasyon
+type = 2 # anim
 
 
 
